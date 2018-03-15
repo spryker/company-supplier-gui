5de32a5efa414fdd83e9fd8d096b7393cb921931
Oleh Hladchenko <glad.oleg@gmail.com>
2018-03-15 14:40:37 +0200

    core-3354 suppliers table.

--- src/Spryker/Zed/CompanySupplierGui/CompanySupplierGuiDependencyProvider.php
+++ src/Spryker/Zed/CompanySupplierGui/CompanySupplierGuiDependencyProvider.php
@@ -14,6 +14,9 @@ use Spryker\Zed\Kernel\Container;
 class CompanySupplierGuiDependencyProvider extends AbstractBundleDependencyProvider
 {
     public const FACADE_COMPANY_SUPPLIER = 'FACADE_COMPANY_SUPPLIER';
+    public const QUERY_CONTAINER_COMPANY_SUPPLIER = 'QUERY_CONTAINER_COMPANY_SUPPLIER';
+    public const FACADE_MONEY = 'FACADE_MONEY';
+    public const FACADE_STORE = 'FACADE_STORE';
 
     /**
      * @param \Spryker\Zed\Kernel\Container $container
@@ -23,6 +26,9 @@ class CompanySupplierGuiDependencyProvider extends AbstractBundleDependencyProvi
     public function provideCommunicationLayerDependencies(Container $container)
     {
         $container = $this->addCompanySupplierFacade($container);
+        $container = $this->addCompanySupplierQueryContainer($container);
+        $container = $this->addMoneyFacade($container);
+        $container = $this->addStoreFacade($container);
 
         return $container;
     }
@@ -36,6 +42,43 @@ class CompanySupplierGuiDependencyProvider extends AbstractBundleDependencyProvi
     {
         $container[static::FACADE_COMPANY_SUPPLIER] = function (Container $container) {
             return new CompanySupplierGuiToCompanySupplierFacadeBridge($container->getLocator()->companySupplier()->facade());
+        };
+
+        return $container;
+    }
+
+    protected function addCompanySupplierQueryContainer(Container $container): Container
+    {
+        $container[static::QUERY_CONTAINER_COMPANY_SUPPLIER] = function (Container $container) {
+            return $container->getLocator()->companySupplier()->queryContainer();
+        };
+
+        return $container;
+    }
+
+    /**
+     * @param \Spryker\Zed\Kernel\Container $container
+     *
+     * @return \Spryker\Zed\Kernel\Container
+     */
+    protected function addMoneyFacade(Container $container): Container
+    {
+        $container[static::FACADE_MONEY] = function (Container $container) {
+            return $container->getLocator()->money()->facade();
+        };
+
+        return $container;
+    }
+
+    /**
+     * @param \Spryker\Zed\Kernel\Container $container
+     *
+     * @return \Spryker\Zed\Kernel\Container
+     */
+    protected function addStoreFacade(Container $container): Container
+    {
+        $container[static::FACADE_STORE] = function (Container $container) {
+            return $container->getLocator()->store()->facade();
         };
 
         return $container;
